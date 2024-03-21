9852066486d11bd595e40e845ff9a9c98551c4cf
amirandagoeller6623 <amirandagoeller6623@student.pps.net>
2024-03-21 15:28:55 -0700

    list done

---
+++ self made thingy.py
@@ -0,0 +1,3 @@
+wincons = ["Skeleton Barrel", "Mortar", "Royal Giant", "Elixir Golem", "Barbarian Barrel", "Hog Rider", "Giant",
+           "Royal Hogs", "Three Musketeers", "Wall Breakers", "Goblin Barrel", "Goblin Drill", "Balloon",
+           "Goblin Giant", "X-Bow", "Electro-Giant", "Golem", "Miner", "Ram Rider", "Graveyard", "Lava Hound"]
